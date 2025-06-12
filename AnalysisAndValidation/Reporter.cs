0b82e288d3b6ffa65928919e9c5c5d10b8bb14f9
Shachar <shar535909@gmail.com>
2025-06-12 12:15:55 +0300

    אז ככה יש לנו 3 תיקיות שמטפלות וכמה דברים כרגע יש לי בעת ההפעלה בMAIN אני מבקש מהמשתמש את השם לו אני מדפיס הודעות הצלחה כוול בדיקה אם קיים כבר במערת בקשה של הדיווח הוצאת פרטים עדכון אנשים כולל מספרים של דיווחים

---
+++ AnalysisAndValidation/Reporter.cs
@@ -0,0 +1,22 @@
+﻿using Project_Malshinon_Communit.AnalysisAndValidation;
+using Project_Malshinon_Communit.DAL;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Malshinon_Communit.NewFolder1
+{
+    internal class Reporter
+    {
+        public static string GetTextFromReporter()
+        {
+            Console.WriteLine("Please enter your report. ");
+            string text = Console.ReadLine();
+            return text;
+        }
+        
+    }
+}
---
+++ Management/Checking.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Project_Malshinon_Communit.DAL;
+
+namespace Project_Malshinon_Communit.Management
+{
+    internal class Checking
+    {
+        
+
+
+
+
+    }
+}
